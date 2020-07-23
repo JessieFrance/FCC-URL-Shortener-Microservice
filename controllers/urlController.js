4ef84917c6e0b72d6412fb9601a3f0e1185b1560
Jessie <FranceJessieJ@gmail.com>
2020-07-23 16:17:49 -0600

    Added code for generating url index and adding an already valid url to the database.

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -1,6 +1,9 @@
 var Url = require('../models/url');
 var async = require('async');
 
+const { check, validationResult } = require('express-validator');
+const { sanitizeBody } = require('express-validator/filter');
+
 /*
 
 Given a url number (urlNumber) stored in the request, this function
@@ -32,7 +35,66 @@ creates a new number for the url shorterner api, or displays errors
 for invalid url input.
 
 */
-exports.url_create_post = function(req, res){
+exports.url_create_post = function(req, res, next){
+
+    // Get (possibly) new url.
+    let new_url = req.body.new_url;
+    
+    // TODO: check if valid url.
+
+    // TODO: check if url already exists in db
+
+    // Create a url index.
+    let urlIndex = new Date().getTime();
+    
+    
+    // Create new url mongoDB document.
+    var newUrldB = new Url( { url: new_url, urlIndex: urlIndex} );
+
+    // Try to save it, or return an error.
+    newUrldB.save(function (err) {
+	if (err){
+	    return next(err);
+	}
+	// New url was saved without an error.
+	// Give json response with new url index.
+	res.json( {"original_url": new_url, "short_url": urlIndex} );
+
+    });
+
+
+    /*
+    let result = req.body;
+    console.log(result);
+    let new_url = req.body.new_url;
+    console.log(new_url);
+    //res.json({'output':'works'});
+    res.json({'output': new_url});
+    */
+	
+};
+
+
+
+
+/*[
+    //check('name','URL name is required.').isLength([{min: 1}]),
+    //sanitizeBody('name').escape(),
+    (req, res, next) => {
+	res.json({'output new': new_url});
+	/*
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+	    return res.status(422).json({ errors: errors.array() });
+	} else{	
+	    res.json({'output new': new_url});
+	}
+
+    }
+];
+*/
+
+/*exports.url_create_post = function(req, res){
 
     let result = req.body;
     console.log(result);
@@ -40,4 +102,4 @@ exports.url_create_post = function(req, res){
     console.log(new_url);
     //res.json({'output':'works'});
     res.json({'output': new_url});
-};
+};*/
